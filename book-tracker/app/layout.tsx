89fb9397416f8bdd373a5ce858608978d03c3eb1
Timo <timokhan2@gmail.com>
2025-09-03 13:01:32 +0200

    frontend components starting point

--- book-tracker/app/layout.tsx
+++ book-tracker/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/frontend/components/footer/Footer";
 import type { Metadata } from "next";
 
 
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Footer />
       </body>
     </html>
   );
