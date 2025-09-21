20bcb53e646a97dc693b28c2cdb9912b56a20624
Timo <timokhan2@gmail.com>
2025-09-21 15:41:30 +0200

    login Signup addes

--- book-tracker/app/page.tsx
+++ book-tracker/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-// import LoginSignup from "@/frontend/components/loginSignup/loginSignup";
-import BookList from "@/frontend/components/bookList/bookList";
+import LoginSignup from "@/frontend/components/loginSignup/loginSignup";
+// import BookList from "@/frontend/components/bookList/bookList";
+// import Homepage from "@/frontend/components/homePage/Homepage";
 
 export default function Home() {
   return (
     <div>
-      <BookList />
-      {/* <LoginSignup /> */}
+      {/* <BookList /> */}
+      <LoginSignup />
+      {/* < Homepage /> */}
     </div>
   );
 };
