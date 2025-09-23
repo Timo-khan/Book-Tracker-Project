574564a078b79adb9b74d78d724ab78beb729471
Timo <timokhan2@gmail.com>
2025-09-23 20:45:50 +0200

    page.tsx set to import only home page

--- book-tracker/app/page.tsx
+++ book-tracker/app/page.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import LoginSignup from "@/frontend/components/loginSignup/loginSignup";
-// import BookList from "@/frontend/components/bookList/bookList";
-// import Homepage from "@/frontend/components/homePage/Homepage";
+import Homepage from "@/frontend/components/homePage/Homepage";
 
 export default function Home() {
   return (
     <div>
-      {/* <BookList /> */}
-      <LoginSignup />
-      {/* < Homepage /> */}
+      < Homepage />
     </div>
   );
 };
