2c6d106636e1a280ff8a40adbdf4427c28acf119
Timo <timokhan2@gmail.com>
2025-09-27 13:16:25 +0200

    types moved to coommonTypes

--- book-tracker/frontend/services/bookServices.ts
+++ book-tracker/frontend/services/bookServices.ts
@@ -1,48 +1,5 @@
-// Shape of a Book object that your app will use internally
-export interface Book {
-	id: string;
-	title: string;
-	authors?: string[];
-	publisher?: string;
-	publishedDate?: string;
-	description?: string;
-	category?: string;
-	thumbnail?: string;
-	webReaderLink?: string;
-	pdfLink?: string;
-	epubLink?: string;
-	previewLink?: string;
-	infoLink?: string;
-}
+import { Book, GoogleBooksResponse  } from "../commonTypes/types";
 
-// Shape of a single book item as returned from Google Books API
-export interface GoogleBookItem {
-	id: string;
-	volumeInfo: {
-		title: string;
-		authors?: string[];
-		imageLinks?: {
-			thumbnail?: string;
-			smallThumbnail?: string;
-		};
-		previewLink?: string;
-		infoLink?: string;
-	};
-	accessInfo?: {
-		webReaderLink?: string;
-		pdf?: {
-			acsTokenLink?: string;
-		};
-		epub?: {
-			acsTokenLink?: string;
-		};
-	};
-}
-
-// Shape of the entire response from a Google Books API search
-export interface GoogleBooksResponse {
-	items?: GoogleBookItem[];
-}
 
 // Force http -> https (keeps undefined as undefined)
 const https = (u?: string) =>
