1f4019021e8edbeb98a0ee28209eec6955ae51a5
Timo <timokhan2@gmail.com>
2025-09-27 12:52:22 +0200

    common types for books and users

--- book-tracker/frontend/commonTypes/types.ts
+++ book-tracker/frontend/commonTypes/types.ts
@@ -1,11 +1,54 @@
+export interface User {
+	id: string;
+	firstName: string;
+	lastName: string;
+	username: string;
+	email: string;
+	image?: string;
+}
+
+// Shape of a Book object that your app will use internally
 export interface Book {
-	_id: string;
+	id: string;
 	title: string;
-	author: string;
+	authors?: string[];
 	publisher?: string;
-	published_date?: string;
+	publishedDate?: string;
 	description?: string;
-	thumbnail_url?: string;
-	isbn?: string;
 	category?: string;
+	thumbnail?: string;
+	webReaderLink?: string;
+	pdfLink?: string;
+	epubLink?: string;
+	previewLink?: string;
+	infoLink?: string;
+}
+
+// Shape of a single book item as returned from Google Books API
+export interface GoogleBookItem {
+	id: string;
+	volumeInfo: {
+		title: string;
+		authors?: string[];
+		imageLinks?: {
+			thumbnail?: string;
+			smallThumbnail?: string;
+		};
+		previewLink?: string;
+		infoLink?: string;
+	};
+	accessInfo?: {
+		webReaderLink?: string;
+		pdf?: {
+			acsTokenLink?: string;
+		};
+		epub?: {
+			acsTokenLink?: string;
+		};
+	};
+}
+
+// Shape of the entire response from a Google Books API search
+export interface GoogleBooksResponse {
+	items?: GoogleBookItem[];
 }
