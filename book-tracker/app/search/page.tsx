dced0d9b2bb6946175d1df7611cc6b9d1d27c196
Timo <timokhan2@gmail.com>
2025-09-28 02:33:15 +0200

    force-dynamic added for npm build

--- book-tracker/app/search/page.tsx
+++ book-tracker/app/search/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic"; // for npm build-Next.js not to try prerendering /search at all (because it always depends on dynamic search input), add:
 
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
